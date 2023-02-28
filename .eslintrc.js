bf23f9ecb2720b81b707474d4e1bd026273d9234
imbharat420 <imbharat420@gmail.com>
2023-02-28 23:42:41 +0530

    test husky hook new

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   plugins: ['react', 'react-native', 'prettier', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   rules: {
     'react/jsx-uses-react': 1,
-    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-body-style': ['error'],
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // import First from './components/First';
 
 // function HomeScreen({navigation}: any) {
